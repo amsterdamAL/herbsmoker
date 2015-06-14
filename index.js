c38bc24636a104ff78251406c356ec2a7c84dc7f
amsterdamAL <amsterdamAL@gmail.com>
2015-06-14 18:18:14 -0400

    heroku adjust more

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var app = require('express').createServer();
-app.get('/', function(req, res) {
-  res.send("Hello World");
-});
-
-
-
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+web: bundle exec puma -C config/puma.rb
