32aa4c7c30036a1564eb2b59615018acda6d06bc
amsterdamAL <amsterdamAL@gmail.com>
2015-06-24 20:21:22 -0400

    opentok

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -8,10 +8,10 @@ class ChatsController < ApplicationController
     @test2 = ENV['api_secret']
     
 
-    @opentok = OpenTok::OpenTok.new 45266732, "141dc7cc7d8eddac8c0a4be35b910ea080936de6"
+    opentok = OpenTok::OpenTok.new "45266732", "141dc7cc7d8eddac8c0a4be35b910ea080936de6"
     
     
-    @session = @opentok.create_session :media_mode => :routed
+    @session = opentok.create_session
     
     @session_id = @session.session_id
     
