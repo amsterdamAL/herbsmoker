9a0716165dfcd8b9e524bd86407910d6131cdb12
amsterdamAL <amsterdamAL@gmail.com>
2015-06-28 23:53:14 -0400

    opentok

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,26 +1,23 @@
 class ChatsController < ApplicationController
   require 'opentok'
+  
+  
   def demo
     
-    @session_id = "2_MX40NTI2NjczMn5-MTQzNTI5NzAxMTQ5N355MGZzTW5TaGgraXpiMjNydTJVOCtOa0Z-UH4"
     
-    @api_key = ENV['api_key']
-    @api_secret = ENV['api_secret']
-    
-
-    opentok = OpenTok::OpenTok.new "45266732", @session_id
+    api_key = ENV['api_key']
+    api_secret = ENV['api_secret']
     
     
+    opentok = OpenTok::OpenTok.new api_key, api_secret
+    
+    session = opentok.create_session
+    @token = session.generate_token
+    @session_id = session.session_id
     
     
-    
-    
-    @token = opentok.generate_token @session_id
-   
   end
   
-  def token
-    @token = opentok.generate_token @session_id
-  end
+  
   
 end
