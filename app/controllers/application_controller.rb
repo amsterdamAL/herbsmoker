db27c8afb7e11cb01c2a8e8e9769212264555483
amsterdamAL <amsterdamAL@gmail.com>
2015-09-11 17:41:59 -0400

    mini register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,13 @@ class ApplicationController < ActionController::Base
 
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
+  
     protected
 
+   
+
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :nickname, :subscriber, :terms, :avatar) }
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :nickname, :subscriber, :terms, :avatar, :e) }
         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar, :nickname, :subscriber, :terms) }
     end
 end
