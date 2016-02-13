22ec81b9cf6e9b0ee5b470f7dd1c5a8dfd5ec645
amsterdamAL <amsterdamAL@gmail.com>
2016-02-13 11:53:40 -0500

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,11 @@
 require 'rubygems'
 require 'sitemap_generator'
 #
-#SitemapGenerator::Sitemap.default_host = 'http://floridatoker.com'
-SitemapGenerator::Sitemap.create do
-  add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
-end
-#SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
+SitemapGenerator::Sitemap.default_host = 'http://agile-stream-2304.herokuapp.com'
+
 
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.floridatoker.com"
+SitemapGenerator::Sitemap.default_host = "http://www.oridatoker.com"
 
 # pick a place safe to write the files
 SitemapGenerator::Sitemap.public_path = 'tmp/'
@@ -22,3 +18,9 @@ SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.ama
 
 # pick a namespace within your bucket to organize your maps
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
+SitemapGenerator::Sitemap.create do
+  add '/home', :changefreq => 'daily', :priority => 0.9
+  add '/contact_us', :changefreq => 'weekly'
+end
+#SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
