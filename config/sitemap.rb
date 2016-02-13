69f278ccfbaf8076ac742121f976d1e4ddca1045
amsterdamAL <amsterdamAL@gmail.com>
2016-02-13 09:20:09 -0500

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'sitemap_generator'
 #
 #SitemapGenerator::Sitemap.default_host = 'http://floridatoker.com'
-#SitemapGenerator::Sitemap.create do
-#  add '/home', :changefreq => 'daily', :priority => 0.9
-#  add '/contact_us', :changefreq => 'weekly'
-#end
+SitemapGenerator::Sitemap.create do
+  add '/home', :changefreq => 'daily', :priority => 0.9
+  add '/contact_us', :changefreq => 'weekly'
+end
 #SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
 
 # Set the host name for URL creation
