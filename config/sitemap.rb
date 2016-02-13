dd4acdf26389a300dcf122f04f974259afd0fdc0
amsterdamAL <amsterdamAL@gmail.com>
2016-02-13 12:00:40 -0500

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,7 +5,7 @@ SitemapGenerator::Sitemap.default_host = 'http://agile-stream-2304.herokuapp.com
 
 
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.oridatoker.com"
+#SitemapGenerator::Sitemap.default_host = "http://www.oridatoker.com"
 
 # pick a place safe to write the files
 SitemapGenerator::Sitemap.public_path = 'tmp/'
@@ -20,7 +20,7 @@ SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.ama
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
-  add '/home', :changefreq => 'daily', :priority => 0.9
+  add '/find', :changefreq => 'daily', :priority => 0.9
   add '/contact_us', :changefreq => 'weekly'
 end
 #SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
