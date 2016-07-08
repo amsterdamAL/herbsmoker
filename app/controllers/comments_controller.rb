3714516a8c50c01938600600689c0da4c66e0cd1
amsterdamAL <amsterdamAL@gmail.com>
2016-07-08 03:52:47 -0400

    comment section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,7 +22,7 @@ class CommentsController < ApplicationController
   
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body, :picurl)
       
     end 
   
