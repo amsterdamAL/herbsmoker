00045262d660436bd4f18919d76cd8517e616e23
amsterdamAL <amsterdamAL@gmail.com>
2016-08-19 15:13:40 -0400

    share

--- config/initializers/shareable.rb
+++ config/initializers/shareable.rb
@@ -12,7 +12,7 @@ Shareable.configure do |config|
   ###############
   ##facebook
   ###############
-  config.app_id='<%= ENV["FACEBOOK_KEY"] %>' # required if using facebook button!
+  #config.app_id='<%= ENV["FACEBOOK_KEY"] %>' # required if using facebook button!
   #config.send= 'false'
   #config.layout='' # 'standard', 'button_count', 'button' or 'box_count'. '' defaults to 'standard'.
   #config.show_faces='' # 'true' or 'false'. '' defaults to 'false'. Don't set to 'true', if kid_directed_site is also 'true'.
